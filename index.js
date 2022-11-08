fb71856898437d6fb4063907b120b9aa7dd93cc4
zperry4 <zactperry73@gmail.com>
2022-11-08 17:42:31 -0500

    Refactor getMovies & displayMovies functions | Also ran prettier for formatting

--- index.js
+++ index.js
@@ -1,64 +1,90 @@
-
 /* Login box */
-var x = document.getElementById('login');
-var y = document.getElementById('register');
-var z = document.getElementById('btn');
+var x = document.getElementById("login");
+var y = document.getElementById("register");
+var z = document.getElementById("btn");
 
 function register() {
-	x.style.left = '-400px';
-	y.style.left = '50px';
-	z.style.left = '110px';
+  x.style.left = "-400px";
+  y.style.left = "50px";
+  z.style.left = "110px";
 }
 
 function login() {
-	x.style.left = '50px';
-	y.style.left = '450px';
-	z.style.left = '0';
+  x.style.left = "50px";
+  y.style.left = "450px";
+  z.style.left = "0";
 }
 
-
 /* This will need to be stored somewhere else at some point for security reasons */
-const API_KEY = 'api_key=bb1d4e0661af455e02af1ea99fb85fcb'; 
-const BASE_URL = 'https://api.themoviedb.org/3/';
+const API_KEY = "api_key=bb1d4e0661af455e02af1ea99fb85fcb";
+const BASE_URL = "https://api.themoviedb.org/3/";
 
 /* This can be formatted to include whatever you want -- 'movie/upcoming' is just a placeholder for now */
-const API_URL = BASE_URL + 'movie/upcoming?' + API_KEY + '&language=en-US&page=1';
+const API_URL =
+  BASE_URL + "movie/upcoming?" + API_KEY + "&language=en-US&page=1";
 
 /* After this URL, add the posterURL return from the API */
-const POSTER_URL = 'https://image.tmdb.org/t/p/original/';
+const POSTER_URL = "https://image.tmdb.org/t/p/original/";
 
 getMovies(API_URL);
 
 /* Makes an API fetch call to get movies with whatever url you want -- this could be for upcoming movies, popular, etc */
+/* This will fetch the URL passed to it and will retrieve a list of movies. It will then loop through each movie, use its ID to construct the DETAIL_URL, and make another API call */
+/* This second call will return even more information about each movie and will call displayMovies on each movie to display them with access to all of the information retrieved */
 function getMovies(url) {
-    fetch(url).then(res => res.json()).then(data => {
-        console.log(data.results);
-        displayMovies(data.results);
-    }).catch(error => {
-        console.log(error);
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      data.results.forEach((movie) => {
+        movieCarousel.innerHTML = "";
+
+        /* Append to this response to get multiple things to return in one request (append_to_response=...) */
+        /* This will get all details, credits, similar movies, and images | Refer to API documentation for other things to append */
+        const DETAIL_URL =
+          BASE_URL +
+          "movie/" +
+          movie.id +
+          "?" +
+          API_KEY +
+          "&language=en-US&append_to_response=videos,credits,similar,images";
+        fetch(DETAIL_URL)
+          .then((res) => res.json())
+          .then((data) => {
+            displayMovies(data);
+          });
+      });
     })
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
-/* Will loop through the data returned by the previous API call & display various pieces of info in HTML */
+/* Passed a movie, which will contain all of the needed information about the individual movie (runtime, videos, etc) */
 function displayMovies(data) {
-    let i = 0;
-    movieCarousel.innerHTML = '';
+  console.log("Movie -- ", data);
+  const {
+    title,
+    poster_path,
+    vote_average,
+    overview,
+    backdrop_path,
+    release_date,
+    runtime,
+  } = data;
+  const backdrop_url = POSTER_URL + backdrop_path;
 
-    data.forEach(movies => {
-        const {title, poster_path, vote_average, overview, backdrop_path} = movies;
-        const backdrop_url = POSTER_URL + backdrop_path;
-        console.log(movies.title);
-  
-        const movieEl = `
+  const movieEl = `
         <div class="carousel-item">
-            <label for="my-modal-${i}" class="btn modal-button" style="height: 300px !important; padding-right: 0px !important; padding-left: 0px !important; margin-right: 10px !important; margin-left: 10px !important; margin-bottom: 10px !important; padding-bottom: 0px !important; width: 200px !important;">
-            <img src="${POSTER_URL + poster_path}" alt="poster" style="margin-right: 0px !important; height: 300px !important; width: 200px !important;">
+            <label for="my-modal-${title}" class="btn modal-button" style="height: 300px !important; padding-right: 0px !important; padding-left: 0px !important; margin-right: 10px !important; margin-left: 10px !important; margin-bottom: 10px !important; padding-bottom: 0px !important; width: 200px !important;">
+            <img src="${
+              POSTER_URL + poster_path
+            }" alt="poster" style="margin-right: 0px !important; height: 300px !important; width: 200px !important;">
             </label>
-            <input type="checkbox" class="modal-toggle" id="my-modal-${i}" />
+            <input type="checkbox" class="modal-toggle" id="my-modal-${title}" />
             <div class="modal">
                 <div class="modal-box bg-gradient-to-t bg-gradient-to-t from-zinc-900 relative w-full max-w-5xl h-full">
                     <label
-                        for="my-modal-${i}"
+                        for="my-modal-${title}"
                         class="btn btn-sm btn-circle absolute right-2 top-2"
                         >✕
                     </label>
@@ -72,7 +98,7 @@ function displayMovies(data) {
                             <h3><b>Overview</b></h3>
                             <p>${overview}</p>
                             <br /><br />
-                            <p class="info"><b>Release Date:</b> ${movies.release_date} | <b>Rating:</b> ${vote_average} / 10</p>
+                            <p class="info"><b>Release Date:</b> ${release_date} | <b>Rating:</b> ${vote_average} / 10 | <b>Runtime:</b> ${runtime} minutes</p>
                         </div>
                     </div>
 
@@ -95,10 +121,8 @@ function displayMovies(data) {
                     </div>
                 </div>
             </div>
-        </div>`
-        movieCarousel.innerHTML += movieEl;
-        i++;
-    })
+        </div>`;
+  movieCarousel.innerHTML += movieEl;
 }
 
 let slideIndex = 1;
@@ -106,41 +130,41 @@ showSlide(slideIndex);
 
 // change slide with the prev/next button
 function moveSlide(moveStep) {
-	showSlide((slideIndex += moveStep));
+  showSlide((slideIndex += moveStep));
 }
 
 // change slide with the dots
 function currentSlide(n) {
-	showSlide((slideIndex = n));
+  showSlide((slideIndex = n));
 }
 
 function showSlide(n) {
-	let i;
-	const slides = document.getElementsByClassName('slide');
-	const dots = document.getElementsByClassName('dot');
+  let i;
+  const slides = document.getElementsByClassName("slide");
+  const dots = document.getElementsByClassName("dot");
 
-	if (n > slides.length) {
-		slideIndex = 1;
-	}
-	if (n < 1) {
-		slideIndex = slides.length;
-	}
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
 
-	// hide all slides
-	for (i = 0; i < slides.length; i++) {
-		slides[i].classList.add('hidden');
-	}
+  // hide all slides
+  for (i = 0; i < slides.length; i++) {
+    slides[i].classList.add("hidden");
+  }
 
-	// remove active status from all dots
-	for (i = 0; i < dots.length; i++) {
-		dots[i].classList.remove('bg-yellow-500');
-		dots[i].classList.add('bg-green-600');
-	}
+  // remove active status from all dots
+  for (i = 0; i < dots.length; i++) {
+    dots[i].classList.remove("bg-yellow-500");
+    dots[i].classList.add("bg-green-600");
+  }
 
-	// show the active slide
-	slides[slideIndex - 1].classList.remove('hidden');
+  // show the active slide
+  slides[slideIndex - 1].classList.remove("hidden");
 
-	// highlight the active dot
-	dots[slideIndex - 1].classList.remove('bg-green-600');
-	dots[slideIndex - 1].classList.add('bg-yellow-500');
+  // highlight the active dot
+  dots[slideIndex - 1].classList.remove("bg-green-600");
+  dots[slideIndex - 1].classList.add("bg-yellow-500");
 }
