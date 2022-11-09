b9dcc052b3cda54e2fce86a404e1607e0ef88566
mpatel65 <mpatel65@vols.utk.edu>
2022-11-09 16:40:37 -0500

    #62 Added hearts to add movies to database from main page

--- index.js
+++ index.js
@@ -1,88 +1,188 @@
+const firebaseConfig = {
+	apiKey: 'AIzaSyDGTFXLcJqWM4F3yWUXc4QalVBkT2Xg6Rg',
+	authDomain: 'suggestify-6b974.firebaseapp.com',
+	databaseURL: 'https://suggestify-6b974-default-rtdb.firebaseio.com',
+	projectId: 'suggestify-6b974',
+	storageBucket: 'suggestify-6b974.appspot.com',
+	messagingSenderId: '287553553595',
+	appId: '1:287553553595:web:c5e8adde4ca305cfaad59e',
+	measurementId: 'G-30270TW59M',
+};
+
+/* Initialize app with firebase firestore */
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
+
+// addMovieToFirestore('Black Adam', '2022/11/7', 'a superhero', 'true');
+async function addMovieToFirestore(
+	movieId,
+	movieTitle,
+	releaseDate,
+	overview,
+	liked
+) {
+	db.collection('My List')
+		.doc(movieTitle)
+		.set({
+			id: movieId,
+			overview: overview,
+			release_date: releaseDate,
+			title: movieTitle,
+			isLiked: liked,
+		})
+		.then(() => {
+			console.log('Document successfully written!', movieTitle);
+		})
+		.catch(error => {
+			console.error('Error writing document: ', error);
+		});
+}
+
+// getMoviesFromFirestore('Terrifier 2');
+async function getMoviesFromFirestore(docName) {
+	var docRef = db.collection('My List').doc(docName);
+
+	docRef
+		.get()
+		.then(doc => {
+			if (doc.exists) {
+				console.log('Document data:', doc.data().isLiked);
+			} else {
+				console.log('No such document!', doc.data());
+			}
+		})
+		.catch(error => {
+			console.log('Error getting document:', error);
+		});
+}
+
+// updateMovieFromFirestore('Black Adam', 'new value');
+async function updateMovieFromFirestore(docName, newValue) {
+	var docRef = db.collection('My List').doc(docName);
+
+	return docRef
+		.update({
+			// only one field at a time can be updated
+			title: newValue,
+			// release_date: 'newValue',
+			// overview: 'newValue',
+			// isLiked: 'newValue',
+		})
+		.then(() => {
+			console.log('Document successfully updated!', docName);
+		})
+		.catch(error => {
+			// The document probably doesn't exist.
+			console.error('Error updating document: ', error);
+		});
+}
+
+// deleteMovieFromFirestore('movie name');
+async function deleteMovieFromFirestore(docName) {
+	db.collection('My List')
+		.doc(docName)
+		.delete()
+		.then(() => {
+			console.log('Document successfully deleted!', docName);
+		})
+		.catch(error => {
+			console.error('Error removing document: ', error);
+		});
+}
+
 /* Login box */
-var x = document.getElementById("login");
-var y = document.getElementById("register");
-var z = document.getElementById("btn");
+var x = document.getElementById('login');
+var y = document.getElementById('register');
+var z = document.getElementById('btn');
 
 function register() {
-  x.style.left = "-400px";
-  y.style.left = "50px";
-  z.style.left = "110px";
+	x.style.left = '-400px';
+	y.style.left = '50px';
+	z.style.left = '110px';
 }
 
 function login() {
-  x.style.left = "50px";
-  y.style.left = "450px";
-  z.style.left = "0";
+	x.style.left = '50px';
+	y.style.left = '450px';
+	z.style.left = '0';
 }
 
 /* This will need to be stored somewhere else at some point for security reasons */
-const API_KEY = "api_key=bb1d4e0661af455e02af1ea99fb85fcb";
-const BASE_URL = "https://api.themoviedb.org/3/";
+const API_KEY = 'api_key=bb1d4e0661af455e02af1ea99fb85fcb';
+const BASE_URL = 'https://api.themoviedb.org/3/';
 
 /* This can be formatted to include whatever you want -- 'movie/upcoming' is just a placeholder for now */
 const API_URL =
-  BASE_URL + "movie/upcoming?" + API_KEY + "&language=en-US&page=1";
+	BASE_URL + 'movie/upcoming?' + API_KEY + '&language=en-US&page=1';
 
 /* After this URL, add the posterURL return from the API */
-const POSTER_URL = "https://image.tmdb.org/t/p/original/";
+const POSTER_URL = 'https://image.tmdb.org/t/p/original/';
 
+let hearts = [];
 getMovies(API_URL);
 
 /* Makes an API fetch call to get movies with whatever url you want -- this could be for upcoming movies, popular, etc */
 /* This will fetch the URL passed to it and will retrieve a list of movies. It will then loop through each movie, use its ID to construct the DETAIL_URL, and make another API call */
 /* This second call will return even more information about each movie and will call displayMovies on each movie to display them with access to all of the information retrieved */
 function getMovies(url) {
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-      data.results.forEach((movie) => {
-        movieCarousel.innerHTML = "";
+	fetch(url)
+		.then(res => res.json())
+		.then(data => {
+			data.results.forEach(movie => {
+				movieCarousel.innerHTML = '';
 
-        /* Append to this response to get multiple things to return in one request (append_to_response=...) */
-        /* This will get all details, credits, similar movies, and images | Refer to API documentation for other things to append */
-        const DETAIL_URL =
-          BASE_URL +
-          "movie/" +
-          movie.id +
-          "?" +
-          API_KEY +
-          "&language=en-US&append_to_response=videos,credits,similar,images";
-        fetch(DETAIL_URL)
-          .then((res) => res.json())
-          .then((data) => {
-            displayMovies(data);
-          });
-      });
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+				/* Append to this response to get multiple things to return in one request (append_to_response=...) */
+				/* This will get all details, credits, similar movies, and images | Refer to API documentation for other things to append */
+				const DETAIL_URL =
+					BASE_URL +
+					'movie/' +
+					movie.id +
+					'?' +
+					API_KEY +
+					'&language=en-US&append_to_response=videos,credits,similar,images';
+				fetch(DETAIL_URL)
+					.then(res => res.json())
+					.then(data => {
+						hearts.push({
+							movieId: data.id,
+							movieTitle: data.title,
+							release: data.release_date,
+							description: data.overview,
+						});
+						displayMovies(data);
+					});
+			});
+		})
+		.catch(error => {
+			console.log(error);
+		});
 }
 
 /* Passed a movie, which will contain all of the needed information about the individual movie (runtime, videos, etc) */
 function displayMovies(data) {
-  console.log("Movie -- ", data);
-  const {
-    title,
-    poster_path,
-    vote_average,
-    overview,
-    backdrop_path,
-    release_date,
-    runtime,
-  } = data;
-  const backdrop_url = POSTER_URL + backdrop_path;
+	let count = 0;
+	const {
+		title,
+		poster_path,
+		vote_average,
+		overview,
+		backdrop_path,
+		release_date,
+		runtime,
+	} = data;
+	const backdrop_url = POSTER_URL + backdrop_path;
 
-  const movieEl = `
-        <div class="carousel-item">
+	const movieEl = `
+        <div class="carousel-item relative">
             <label for="my-modal-${title}" class="btn modal-button" style="height: 300px !important; padding-right: 0px !important; padding-left: 0px !important; margin-right: 10px !important; margin-left: 10px !important; margin-bottom: 10px !important; padding-bottom: 0px !important; width: 200px !important;">
             <img src="${
-              POSTER_URL + poster_path
-            }" alt="poster" style="margin-right: 0px !important; height: 300px !important; width: 200px !important;">
+							POSTER_URL + poster_path
+						}" alt="poster" style="margin-right: 0px !important; height: 300px !important; width: 200px !important;">
             </label>
+            <i class="heart-icon fa-regular fa-heart absolute right-8 bottom-[230px] text-4xl text-white hover: cursor-pointer" aria-hidden="true"></i>
             <input type="checkbox" class="modal-toggle" id="my-modal-${title}" />
             <div class="modal">
-                <div class="modal-box bg-gradient-to-t bg-gradient-to-t from-zinc-900 relative w-full max-w-5xl h-full">
+                <div class="modal-box bg-gradient-to-t from-zinc-900 relative w-full max-w-5xl h-full">
                     <label
                         for="my-modal-${title}"
                         class="btn btn-sm btn-circle absolute right-2 top-2"
@@ -122,7 +222,33 @@ function displayMovies(data) {
                 </div>
             </div>
         </div>`;
-  movieCarousel.innerHTML += movieEl;
+	movieCarousel.innerHTML += movieEl;
+
+	/* Heart functionality */
+	let hIcon = document.querySelectorAll('.heart-icon');
+	hIcon.forEach((icon, index) => {
+		icon.addEventListener('click', () => {
+			if (icon.classList.contains('fa-regular')) {
+				icon.classList.remove('fa-regular');
+				icon.classList.add('fa-solid');
+				addMovieToFirestore(
+					hearts[index].movieId,
+					hearts[index].movieTitle,
+					hearts[index].release,
+					hearts[index].description,
+					'true'
+				);
+				count++;
+				document.getElementsByClassName('badge')[0].innerHTML = count;
+			} else {
+				icon.classList.remove('fa-solid');
+				icon.classList.add('fa-regular');
+				deleteMovieFromFirestore(hearts[index].movieTitle);
+				count--;
+				document.getElementsByClassName('badge')[0].innerHTML = count;
+			}
+		});
+	});
 }
 
 let slideIndex = 1;
@@ -130,41 +256,41 @@ showSlide(slideIndex);
 
 // change slide with the prev/next button
 function moveSlide(moveStep) {
-  showSlide((slideIndex += moveStep));
+	showSlide((slideIndex += moveStep));
 }
 
 // change slide with the dots
 function currentSlide(n) {
-  showSlide((slideIndex = n));
+	showSlide((slideIndex = n));
 }
 
 function showSlide(n) {
-  let i;
-  const slides = document.getElementsByClassName("slide");
-  const dots = document.getElementsByClassName("dot");
+	let i;
+	const slides = document.getElementsByClassName('slide');
+	const dots = document.getElementsByClassName('dot');
 
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
+	if (n > slides.length) {
+		slideIndex = 1;
+	}
+	if (n < 1) {
+		slideIndex = slides.length;
+	}
 
-  // hide all slides
-  for (i = 0; i < slides.length; i++) {
-    slides[i].classList.add("hidden");
-  }
+	// hide all slides
+	for (i = 0; i < slides.length; i++) {
+		slides[i].classList.add('hidden');
+	}
 
-  // remove active status from all dots
-  for (i = 0; i < dots.length; i++) {
-    dots[i].classList.remove("bg-yellow-500");
-    dots[i].classList.add("bg-green-600");
-  }
+	// remove active status from all dots
+	for (i = 0; i < dots.length; i++) {
+		dots[i].classList.remove('bg-yellow-500');
+		dots[i].classList.add('bg-green-600');
+	}
 
-  // show the active slide
-  slides[slideIndex - 1].classList.remove("hidden");
+	// show the active slide
+	slides[slideIndex - 1].classList.remove('hidden');
 
-  // highlight the active dot
-  dots[slideIndex - 1].classList.remove("bg-green-600");
-  dots[slideIndex - 1].classList.add("bg-yellow-500");
+	// highlight the active dot
+	dots[slideIndex - 1].classList.remove('bg-green-600');
+	dots[slideIndex - 1].classList.add('bg-yellow-500');
 }
