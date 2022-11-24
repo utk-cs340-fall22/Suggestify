7dd6bbb4a898a00e434d764e798742c1dc3a825b
mpatel65 <mpatel65@vols.utk.edu>
2022-11-23 21:10:28 -0500

    my-list page now fetches from firestore

---
+++ my-list.js
@@ -0,0 +1,206 @@
+const firebaseConfig = {
+	apiKey: 'AIzaSyDGTFXLcJqWM4F3yWUXc4QalVBkT2Xg6Rg',
+	authDomain: 'suggestify-6b974.firebaseapp.com',
+	databaseURL: 'https://suggestify-6b974-default-rtdb.firebaseio.com',
+	projectId: 'suggestify-6b974',
+	storageBucket: 'suggestify-6b974.appspot.com',
+	messagingSenderId: '287553553595',
+	appId: '1:287553553595:web:c5e8adde4ca305cfaad59e',
+	measurementId: 'G-30270TW59M',
+};
+
+/* Initialize app with firebase firestore */
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
+
+// addMovieToFirestore('Black Adam', '2022/11/7', 'a superhero', 'true');
+async function addMovieToFirestore(
+	movieId,
+	movieTitle,
+	releaseDate,
+	overview,
+	liked
+) {
+	db.collection('My List')
+		.doc(movieTitle)
+		.set({
+			id: movieId,
+			overview: overview,
+			release_date: releaseDate,
+			title: movieTitle,
+			isLiked: liked,
+		})
+		.then(() => {
+			console.log('Document successfully written - 1!', movieTitle);
+		})
+		.catch(error => {
+			console.error('Error writing document: ', error);
+		});
+}
+
+// getMoviesFromFirestore('Terrifier 2');
+async function getMoviesFromFirestore(docName) {
+	var docRef = db.collection('My List').doc(docName);
+
+	docRef
+		.get()
+		.then(doc => {
+			if (doc.exists) {
+				console.log('Document data:', doc.data().isLiked);
+			} else {
+				console.log('No such document!', doc.data());
+			}
+		})
+		.catch(error => {
+			console.log('Error getting document:', error);
+		});
+}
+
+// updateMovieFromFirestore('Black Adam', 'new value');
+async function updateMovieFromFirestore(docName, newValue) {
+	var docRef = db.collection('My List').doc(docName);
+
+	return docRef
+		.update({
+			// only one field at a time can be updated
+			title: newValue,
+			// release_date: 'newValue',
+			// overview: 'newValue',
+			// isLiked: 'newValue',
+		})
+		.then(() => {
+			console.log('Document successfully updated!', docName);
+		})
+		.catch(error => {
+			// The document probably doesn't exist.
+			console.error('Error updating document: ', error);
+		});
+}
+
+// deleteMovieFromFirestore('movie name');
+async function deleteMovieFromFirestore(docName) {
+	db.collection('My List')
+		.doc(docName)
+		.delete()
+		.then(() => {
+			console.log('Document successfully deleted!', docName);
+		})
+		.catch(error => {
+			console.error('Error removing document: ', error);
+		});
+}
+
+// take a snapshot of all liked movies from firestore
+getLikedMoviesFromFirestore();
+async function getLikedMoviesFromFirestore() {
+	db.collection('My List')
+		.where('isLiked', '==', 'true')
+		.get()
+		.then(querySnapshot => {
+			querySnapshot.forEach(doc => {
+				// doc.data() is never undefined for query doc snapshots
+				getLikedMoviesFromApi(doc.data());
+			});
+		})
+		.catch(error => {
+			console.log('Error getting documents: ', error);
+		});
+}
+
+
+const API_KEY = 'api_key=bb1d4e0661af455e02af1ea99fb85fcb';
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const POSTER_URL = 'https://image.tmdb.org/t/p/original/';
+
+
+// fetch movie by id from firestore
+function getLikedMoviesFromApi(movieInfo) {
+	const { id } = movieInfo;
+
+	const movieUrl = `${BASE_URL}movie/${id}?${API_KEY}&language=en-US`;
+
+	fetch(movieUrl)
+		.then(res => res.json())
+		.then(data => displayMoviesFromFirestore(data));
+}
+
+// display movies like movies page
+function displayMoviesFromFirestore(data) {
+	const {
+		title,
+		release_date,
+		overview,
+		poster_path,
+		backdrop_path,
+		vote_average,
+		runtime,
+		revenue,
+	} = data;
+
+	const backdropURL = POSTER_URL + backdrop_path;
+
+	const movieCard = document.getElementById('movieCard');
+	const movieEl = document.createElement('div');
+	movieEl.classList.add('movie');
+
+	movieEl.innerHTML = `
+    <label for="${title}" class="btn modal-button" style="height: 400px !important; padding-right: 0px !important; padding-left: 0px !important; margin-right: 10px !important; margin-left: 10px !important; margin-bottom: 10px !important; padding-bottom: 0px !important; width: 250px !important;">
+        <img src="${
+					POSTER_URL + poster_path
+				}" alt="poster" style="margin-right: 0px !important; height: 400px !important; width: 250px !important;">
+        </label>
+        <i class="heart-icon fa-solid fa-heart relative bottom-[4rem] right-[4rem] text-4xl text-white hover: cursor-pointer" aria-hidden="true"></i>
+        <input type="checkbox" id="${title}" class="modal-toggle" />
+        <div class="modal">
+          <div class="modal-box w-full max-w-5xl h-full">
+            <div class="card w-96 bg-base-100 shadow-xl image-full" style="width: 970px !important; height: 400px !important;">
+              <figure>            
+                <img src="${backdropURL}" alt="backDrop" style="width: 970px !important; height: 500px !important; margin-right: 0px !important; border-radius: 0px !important; border-width: 0px !important; padding: 1px 0px 1px 1px !important;"></img>
+              </figure>
+              <div class="card-body">
+                <h1 class="card-title" style="text-align: center !important;">
+                <font size="+100">${title}</font> 
+                </h1>
+                <br/>
+                <h3> <b> Overview </b> </h3>
+                <p>${overview}</p>
+                <br/><br/>
+                <p><b>Release Date:</b> ${release_date} | <b>Rating:</b> ${vote_average} / 10 | <b>Runtime:</b> ${runtime} minutes</p>
+              </div>
+            </div>
+            <div class="flex justify-center w-full py-2 gap-2">
+              <a href="#item1${title}" class="btn btn-xs">More Info</a> 
+              <a href="#item2${title}" class="btn btn-xs">See Also</a> 
+              <a href="#item3${title}" class="btn btn-xs">Reviews</a> 
+            </div>
+          <div class="carousel w-full">
+            <div id="item1${title}" class="carousel-item w-full">
+              <div class="card w-96 bg-base-100 shadow-xl">
+                <div class="card-body">
+                  <h1><b>More Info</b></h1>
+                  <p>Revenue: $${revenue}</p>
+                </div>
+              </div>
+            </div> 
+            <div id="item2${title}" class="carousel-item w-full">
+              <div class="card w-96 bg-base-100 shadow-xl">
+                <div class="card-body">
+                <h1><b>See Also</b></h1>
+                </div>
+              </div>
+            </div> 
+            <div id="item3${title}" class="carousel-item w-full">
+            <div class="card w-96 bg-base-100 shadow-xl">
+              <div class="card-body">
+              <h1><b>Reviews</b></h1>
+              </div>
+            </div>
+          </div> 
+        </div> 
+        <div class="modal-action">
+          <label for="${title}" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+        </div>
+      </div>
+    </div>`;
+	movieCard.appendChild(movieEl);
+}
